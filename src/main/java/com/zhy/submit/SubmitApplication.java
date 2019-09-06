60c62cdf927ba757cebb546745cfef1a9503931d
wtSeptember <2265981875@qq.com>
2019-09-06 17:11:52 +0800

    Initial commit

---
+++ src/main/java/com/zhy/submit/SubmitApplication.java
@@ -0,0 +1,13 @@
+package com.zhy.submit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmitApplication.class, args);
+    }
+
+}
