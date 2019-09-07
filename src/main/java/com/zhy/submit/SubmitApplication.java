6ee6c3efcc5b9e8c80f605db37a6f4ce06ba54f5
wtSeptember <2265981875@qq.com>
2019-09-07 12:25:43 +0800

    第一次提交

--- src/main/java/com/zhy/submit/SubmitApplication.java
+++ src/main/java/com/zhy/submit/SubmitApplication.java
@@ -2,12 +2,16 @@ package com.zhy.submit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class SubmitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubmitApplication.class, args);
+
+        }
     }
 
-}
+
